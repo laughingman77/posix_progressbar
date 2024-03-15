1737dc20c22dfefeea0e2ff0ad1b9c8aab044808
lauhging man <8545282-laughing_man77@users.noreply.gitlab.com>
2024-03-15 20:28:10 +1100

    Minor fixes.

--- example_posix.sh
+++ example_posix.sh
@@ -8,7 +8,7 @@ total=10
 while [ "$done_so_far" -lt "$total" ]; do
       done_so_far=$((done_so_far + 1))
       todo=$((total - done_so_far))
-      progressbar --bar-width=50 "$done_so_far" "$total" "$todo left to complete" >&2
+      progressbar --bar-width=50 "$done_so_far" "$total" "$todo left to complete"
       sleep 1
 done
 echo >&2
